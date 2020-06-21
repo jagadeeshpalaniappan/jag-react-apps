c7683b8c5058c69316f5cf51a3b6c76358f3fe89
Jagadeesh <jagadeeshthegeek@gmail.com>
2020-06-21 01:00:43 -0700

    cu

--- level1/1-react-redux/src/modules/user/components/UserFiltersModal.js
+++ level1/1-react-redux/src/modules/user/components/UserFiltersModal.js
@@ -1,25 +1,7 @@
 import PropTypes from "prop-types";
-import React, { useEffect } from "react";
+import React from "react";
 import { AppCard, AppModal } from "../../common/components";
 import UserFiltersModalForm from "./UserFiltersModalForm";
-
-/* 
-const ToggleSwitch = ({ checked, onChange }) => {
-  return (
-    <div className="custom-control custom-switch">
-      <input
-        type="checkbox"
-        className="custom-control-input"
-        id="customSwitches"
-        checked={checked}
-        onChange={onChange}
-        readOnly
-      />
-      <label className="custom-control-label" htmlFor="customSwitches"></label>
-    </div>
-  );
-};
- */
 
 const UserFiltersModal = ({ filters, isOpen, onOk, onCancel }) => {
   console.log("### UserFiltersModal:", { filters });
