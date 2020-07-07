99ebca664a6818d9da1a4075fd0f13665ffb409f
Jagadeesh <jagadeeshthegeek@gmail.com>
2020-07-07 00:03:44 -0700

    fixed: react-select default loading options

--- level1/3-react-context-apollo/src/modules/post/components/UsersDropdown.js
+++ level1/3-react-context-apollo/src/modules/post/components/UsersDropdown.js
@@ -77,6 +77,8 @@ const UsersDropdown = memo(function ({
       isMulti={isMulti}
       value={selectedUsers}
       onChange={onChange}
+      cacheOptions
+      defaultOptions
       loadOptions={getUserDropDownOptionsAsync}
     />
   );
